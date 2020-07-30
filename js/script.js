c4f293557258b405bdbee322eccd445e32687d6f
shinohara03 <vbz.erika.shinohara@gmail.com>
2020-07-30 16:10:20 +0900

    ハンバーガーメニュー作成7/30

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 "use stict";
 
-{
-    const btn = document.getElementById("btn");
-
-
-
+function hamburger_line(){
+    document.getElementById('line_1').classList.toggle('line_1')
+    document.getElementById('line_2').classList.toggle('line_2')
+    document.getElementById('line_3').classList.toggle('line_3')
+    document.getElementById('nav_hamburger').classList.toggle('in');
 }
+
+document.getElementById('hamburger_line').addEventListener('click', function () {
+    hamburger_line();
+});
