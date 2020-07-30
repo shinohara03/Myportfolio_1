5e66c88fb9a0ad229c826c97a6c13bc975c8eafd
shinohara03 <vbz.erika.shinohara@gmail.com>
2020-07-30 17:17:24 +0900

    ハンバーガーメニュー修正7/30

--- js/script.js
+++ js/script.js
@@ -4,9 +4,10 @@ function hamburger_line(){
     document.getElementById('line_1').classList.toggle('line_1')
     document.getElementById('line_2').classList.toggle('line_2')
     document.getElementById('line_3').classList.toggle('line_3')
-    document.getElementById('nav_hamburger').classList.toggle('in');
+    document.getElementById('nav_hamburger').classList.toggle('fadein');
 }
 
 document.getElementById('hamburger_line').addEventListener('click', function () {
     hamburger_line();
 });
+
